0ff1db8c75118e4604d689aa2cc1aeea0c09e7c3
Bryushkov Ilya <bryushkov.i@yandex.ru>
2024-06-14 12:50:17 +0300

    Собирается, но без точки входа

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Animals.Presenter;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,7 @@ namespace Animals
     /// </summary>
     public partial class App : Application
     {
+
     }
 
 }
